68d6898ee34214b2a77732fa7474a45636dfcee7
SimonRuaGitHub <simonfruavargas@gmail.com>
2017-11-21 20:18:32 -0500

    first commit project with src and config files

---
+++ src/main/java/com/mycompany/testexpositor/services/UsuarioServices.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testexpositor.services;
+
+import com.mycompany.testexpositor.contract.UsuarioContract;
+
+/**
+ *
+ * @author simon.rua
+ */
+public class UsuarioServices implements UsuarioContract
+{
+
+    public String dummy() 
+    {
+           return "conexion BackEnd exitosa";
+    }
+
+
+    
+}
